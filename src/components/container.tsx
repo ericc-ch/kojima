fa67bdb8763b0689e6e5d3af61a505dd00ff8918
Erick Christian <erickchristian48@gmail.com>
2025-03-12 09:25:15 +0700

    feat: Remove modal and add textarea for chatting

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -20,21 +20,6 @@ export function Container() {
           </div>
         </div>
 
-        <button class="btn" onclick="my_modal_1.showModal()">
-          open modal
-        </button>
-        <dialog class="modal" id="my_modal_1">
-          <div class="modal-box">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Press ESC key or click the button below to close</p>
-            <div class="modal-action">
-              <form method="dialog">
-                <button class="btn">Close</button>
-              </form>
-            </div>
-          </div>
-        </dialog>
-
         <textarea
           class="textarea mt-auto w-full resize-none"
           placeholder="Chat with Kojima"
